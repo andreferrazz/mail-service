d9f16767105ceeb255b61993e30265e0cb16624f
André Ferraz <oandreferraz@gmail.com>
2025-04-20 18:24:54 -0300

    First commit

---
+++ src/test/java/com/andreferraz/mail_sender/MailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreferraz.mail_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
